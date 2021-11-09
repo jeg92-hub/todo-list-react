08941752fcfe4eb2698a467b278cacfd988e0c95
joneganagranado <joneganagranado@hotmail.com>
2021-11-09 18:54:49 +0000

    terminar con todo-list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
+const Todo = () => {
+	const [list, setList] = useState([]);
+	const [currentTask, setCurrentTask] = useState("");
+
+	function onkeydown(e) {
+		if (e.key === "Enter") {
+			addItem();
+		}
+	}
+
+	function addItem() {
+		const newList = [...list, currentTask];
+		setList(newList);
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card">
+			<input
+				type="text"
+				placeholder="Añadir tarea"
+				onChange={e => {
+					setCurrentTask(e.target.value);
+				}}
+				onKeyDown={onkeydown}
+				value={currentTask}
+			/>
+			{list.map((item, index) => (
+				<div className="item" key={index}>
+					<p>{item}</p>
+				</div>
+			))}
 		</div>
 	);
 };
 
-export default Home;
+export default Todo;
